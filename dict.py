5960549fbad5c4726dd3f78b84b8fb05e2e00d8d
sirireddy54 <sirireddy229@gmail.com>
2025-01-07 14:44:10 -0600

    initally pushing

---
+++ dict.py
@@ -0,0 +1,26 @@
+#dictionaries are Key: Value Pairs
+# It will print values for keys
+#keys should be unique
+
+
+dictionary = {
+   "name": "siri",
+   "age": 25,
+   "subject": "Telugu",
+   "is_clever": True,
+   "grades_list": ['A','A-','A+','B+']
+}
+
+print(dictionary)
+
+print (dictionary["name"])
+#print(dictionary["gender"])       #error
+
+print(dictionary.get("subject"))
+print(dictionary.get("gender"))    #prints "None", no error
+
+dictionary["gender"]= "female"  #new Key:Value Pair
+print(dictionary["gender"])
+
+dictionary["name"]="chinnu"   #reassigning new Value for Key "name"
+print(dictionary["name"])   #prints new value i.e Chinnu not Siri
---
+++ dict_exp.py
@@ -0,0 +1,12 @@
+numbers= {1:"one", 
+          2:"two",
+         3: "three",
+         4: "four",
+         5: "five",
+         6: "six",
+         7: "seven",
+         8: "eight",
+         9: "nine",
+         10: "ten" }
+
+phone= int(input("enter phone number: "))
